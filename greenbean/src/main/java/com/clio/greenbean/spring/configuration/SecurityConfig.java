fa81b7fe21d3fcd8febb18cf155bb1eac4d9ce31
ClioChang <45628349+ClioChang@users.noreply.github.com>
2019-10-14 11:23:06 +0800

    SecurityConfig.java -> config(HttpSecurity http)

--- greenbean/src/main/java/com/clio/greenbean/spring/configuration/SecurityConfig.java
+++ greenbean/src/main/java/com/clio/greenbean/spring/configuration/SecurityConfig.java
@@ -1,19 +1,37 @@
 package com.clio.greenbean.spring.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * created by 吾乃逆世之神也 on 2019/10/13
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/resources/**", "/signup", "/about","/signIn").permitAll()
+        .anyRequest().authenticated()
+        .and().formLogin().loginPage("/login").permitAll()
+        .defaultSuccessUrl("/home");
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+    
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
