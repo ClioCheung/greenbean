eb8c6ec29896392031e5aacf25bb3aed0f3667dd
ClioChang <cliostill1@gmail.com>
2019-10-15 11:24:19 +0800

    配置 dataSource

--- greenbean/src/main/java/com/clio/greenbean/spring/configuration/RootConfig.java
+++ greenbean/src/main/java/com/clio/greenbean/spring/configuration/RootConfig.java
@@ -1,8 +1,12 @@
 package com.clio.greenbean.spring.configuration;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+
+import javax.sql.DataSource;
 
 /**
  * created by 吾乃逆世之神也 on 2019/10/11
@@ -17,4 +21,13 @@ import org.springframework.context.annotation.FilterType;
 )
 public class RootConfig {
 
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/greenbean");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
 }
--- greenbean/src/main/java/com/clio/greenbean/spring/configuration/SecurityConfig.java
+++ greenbean/src/main/java/com/clio/greenbean/spring/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.clio.greenbean.spring.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.sql.DataSource;
 
 /**
  * created by 吾乃逆世之神也 on 2019/10/13
@@ -16,18 +18,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
+    public static final String QUERY_USER_SQL = "select username,password,enabled from t_user where username = ?";
+    public static final String QUERY_AUTHORITIES_SQL = "select username,authority from t_authority inner join t_user on t_authority.user_id = t_user.id where username = ?";
+    
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private DataSource dataSource;
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
         .antMatchers("/resources/**", "/signup", "/about","/signIn").permitAll()
         .anyRequest().authenticated()
-        .and().formLogin().loginPage("/login").permitAll()
+        .and().formLogin().loginPage("/").permitAll()
         .defaultSuccessUrl("/home");
     }
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.jdbcAuthentication().dataSource(dataSource)
+        .usersByUsernameQuery(QUERY_USER_SQL)
+        .authoritiesByUsernameQuery(QUERY_AUTHORITIES_SQL);
     }
     
     @Bean
