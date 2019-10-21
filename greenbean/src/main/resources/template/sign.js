2f0e6f469809ba618a220495441a4a6e6535e4a8
ClioChang <cliostill1@gmail.com>
2019-10-22 07:21:10 +0800

    处理sign in form和sign up form

--- greenbean/src/main/resources/template/sign.js
+++ greenbean/src/main/resources/template/sign.js
@@ -1,34 +1,48 @@
 (function(){
     $(document).ready(function () {
         var activeClassName = "active";
+        var hideFormName = "hideForm";
         var signInTab = $("#signInTab");
         var signUpTab = $("#signUpTab");
-        var submitButton = $("#submitButton");
-        var tabArray = new Array(signInTab,signUpTab);
+        var signInForm = $("#signInForm");
+        var signUpForm = $("#signUpForm");
+
+        var signInObject =  {
+            tab: signInTab,
+            form: signInForm
+        }
+
+        var signUpObject =  {
+            tab: signUpTab,
+            form: signUpForm
+        }
+
+        var signObjectArray = new Array(signInObject,signUpObject);
 
         var handlerDataObject = {
             activeClassName: activeClassName,
-            tabArray: tabArray,
-            submitButton: submitButton
+            hideFormName: hideFormName,
+            signObjectArray: signObjectArray
         }
 
         var tabOnClickHandler = function(event) {
-            var handlerDataObject = event.data;
+            handlerDataObject = event.data;
             var tabDomObject = $(event.target);
-            for (var i = 0; i < tabArray.length; i++) {
-                if(tabArray[i].is(tabDomObject)){
+            for (var i = 0; i < signObjectArray.length; i++) {
+                if(signObjectArray[i].tab.is(tabDomObject)){
                     if(!tabDomObject.hasClass(handlerDataObject.activeClassName)) {
                         tabDomObject.addClass(handlerDataObject.activeClassName);
-                        handlerDataObject.submitButton.text(tabDomObject.text());
-                    } else {
-                        tabDomObject.removeClass(handlerDataObject.activeClassName);
+                        signObjectArray[i].form.removeClass(handlerDataObject.hideFormName);
                     }
+                }else {
+                    signObjectArray[i].tab.removeClass(handlerDataObject.activeClassName);
+                    signObjectArray[i].form.addClass(handlerDataObject.hideFormName);
                 }
             }
         }
 
-        for (var i = 0; i < tabArray.length ; i++) {
-            tabArray[i].click(handlerDataObject,tabOnClickHandler);
+        for (var i = 0; i < signObjectArray.length ; i++) {
+            signObjectArray[i].tab.click(handlerDataObject,tabOnClickHandler);
         }
 
     });
