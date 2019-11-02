171f7225e05d245f4bf6cc6aeecea8cc76f6e6fd
ClioChang <cliostill1@gmail.com>
2019-11-02 21:53:08 +0800

    单元测试 insertUser()测试null输入参数

--- greenbean/src/test/java/com/clio/greenbean/spring/service/UserServiceTest.java
+++ greenbean/src/test/java/com/clio/greenbean/spring/service/UserServiceTest.java
@@ -2,6 +2,8 @@ package com.clio.greenbean.spring.service;
 
 import com.clio.greenbean.domain.User;
 import com.clio.greenbean.mybatis.mapper.UserMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
@@ -9,30 +11,33 @@ import org.mockito.Mockito;
 /**
  * created by 吾乃逆世之神也 on 2019/11/1
  */
-public class UserServiceTest {
+class UserServiceTest {
+    private static UserService userService;
+    private static UserMapper mockUserMapper;
+    
+    @BeforeAll
+    public static void setUp(){
+        mockUserMapper = Mockito.mock(UserMapper.class);
+        userService = new UserService(mockUserMapper);
+    }
     
     @Test
-    void testInsertUser() throws NoSuchFieldException, IllegalAccessException {
-        UserMapper mockUserMapper = Mockito.mock(UserMapper.class);
-        UserService userService = new UserService(mockUserMapper);
+    void testInsertUser(){
         User mockUser = Mockito.mock(User.class);
-        
-/*      利用反射机制获取UserService.class的属性UserMapper
-//      环境搭建
-        UserService userService = new UserService();
-        User mockUser = Mockito.mock(User.class);
-//      依赖UserMapper
-        UserMapper mockUserMapper = Mockito.mock(UserMapper.class);
-        Class<UserService> UserServiceClass = UserService.class;
-        Field userMapperField = UserServiceClass.getDeclaredField("userMapper");
-        userMapperField.setAccessible(true);
-        userMapperField.set(userService,mockUserMapper);*/
-        
         userService.insertUser(mockUser);
 //      验证调用顺序
         InOrder inOrder = Mockito.inOrder(mockUserMapper);
         inOrder.verify(mockUserMapper).insertUserBasicInfo(mockUser);
         inOrder.verify(mockUserMapper).insertUserAuthority(mockUser.getId(),mockUser.getAuthority());
+    }
+    
+    @Test
+    void TestInsertUserWithNull(){
+        Assertions.assertThrows(IllegalArgumentException.class,() -> {
+            userService.insertUser(null);
+        });
+        Mockito.verify(mockUserMapper,Mockito.never()).insertUserBasicInfo(Mockito.any());
+        Mockito.verify(mockUserMapper,Mockito.never()).insertUserAuthority(Mockito.any(),Mockito.any());
     }
     
 }
