fc3b59e84736550c0226bbf882ce2cff291805a4
ClioChang <cliostill1@gmail.com>
2019-11-05 15:51:50 +0800

    完善testInsertUser()测试方法

--- greenbean/src/test/java/com/clio/greenbean/spring/service/UserServiceTest.java
+++ greenbean/src/test/java/com/clio/greenbean/spring/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 
 /**
  * created by 吾乃逆世之神也 on 2019/11/1
@@ -17,6 +18,11 @@ class UserServiceTest {
     @BeforeAll
     static void setUp(){
         mockUserMapper = Mockito.mock(UserMapper.class);
+        Mockito.doAnswer((InvocationOnMock invocation)->{
+            User mockUser = invocation.getArgument(0);
+            Mockito.when(mockUser.getId()).thenReturn(666);
+            return null;
+        }).when(mockUserMapper).insertUserBasicInfo(Mockito.any(User.class));
         userService = new UserService(mockUserMapper);
     }
     
@@ -27,6 +33,6 @@ class UserServiceTest {
 //      验证调用顺序
         InOrder inOrder = Mockito.inOrder(mockUserMapper);
         inOrder.verify(mockUserMapper).insertUserBasicInfo(mockUser);
-        inOrder.verify(mockUserMapper).insertUserAuthority(mockUser.getId(),mockUser.getAuthority());
+        inOrder.verify(mockUserMapper).insertUserAuthority(666,mockUser.getAuthority());
     }
 }
