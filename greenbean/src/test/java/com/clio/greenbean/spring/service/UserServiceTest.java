f40f346803e2cbd2b07887f1ecf9e91ed98e1602
ClioChang <cliostill1@gmail.com>
2019-11-06 17:08:07 +0800

    完善testInsertUser方法

--- greenbean/src/test/java/com/clio/greenbean/spring/service/UserServiceTest.java
+++ greenbean/src/test/java/com/clio/greenbean/spring/service/UserServiceTest.java
@@ -10,10 +10,15 @@ import org.mockito.InOrder;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 
+import java.util.List;
+
 /**
  * created by 吾乃逆世之神也 on 2019/11/1
  */
 class UserServiceTest {
+    private static String existedUsername = "exist";
+    private static String notExistedUsername = "notExist";
+    
     private static UserService userService;
     private static UserMapper mockUserMapper;
     
@@ -25,45 +30,52 @@ class UserServiceTest {
     
     @Test
     void testInsertUser(){
+        int mockUserId = 666;
 //        使用spy
 //        userService = Mockito.spy(userService);
-//        Mockito.doReturn(true).when(userService).validateUsernameDuplicated("b");
+//        Mockito.doReturn(true).when(userService).validateUsernameDuplicated(notExistedUsername);
 //       不使用spy
-        Mockito.when(mockUserMapper.getUserByUsername("b")).thenReturn(null);
-        
+        Mockito.when(mockUserMapper.getUserByUsername(notExistedUsername)).thenReturn(null);
+        // 返回模仿的用户ID ： mockUserId
         Mockito.doAnswer((InvocationOnMock invocation)->{
             User mockUser = invocation.getArgument(0);
-            Mockito.when(mockUser.getId()).thenReturn(666);
+            Mockito.when(mockUser.getId()).thenReturn(mockUserId);
             return null;
         }).when(mockUserMapper).insertUserBasicInfo(Mockito.any(User.class));
+        
         User mockUser = Mockito.mock(User.class);
-        Mockito.when(mockUser.getUsername()).thenReturn("b");
+        Mockito.when(mockUser.getUsername()).thenReturn(notExistedUsername);
+        // 返回模仿的权限 ： mockAuthority
+        List<String> mockAuthority = Mockito.mock(List.class);
+        Mockito.when(mockAuthority.get(0)).thenReturn("USER");
+        Mockito.when(mockUser.getAuthority()).thenReturn(mockAuthority);
+        
         userService.insertUser(mockUser);
         // 验证调用顺序
         InOrder inOrder = Mockito.inOrder(mockUserMapper);
         inOrder.verify(mockUserMapper).insertUserBasicInfo(mockUser);
-        inOrder.verify(mockUserMapper).insertUserAuthority(666,mockUser.getAuthority());
+        inOrder.verify(mockUserMapper).insertUserAuthority(mockUserId,mockAuthority);
     }
     
     @Test
     void testInsertUserWithDuplicatedUsername() {
-        Mockito.when(mockUserMapper.getUserByUsername("a")).thenReturn(Mockito.mock(User.class));
+        Mockito.when(mockUserMapper.getUserByUsername(existedUsername)).thenReturn(Mockito.mock(User.class));
         User mockUser =  Mockito.mock(User.class);
-        Mockito.when(mockUser.getUsername()).thenReturn("a");
+        Mockito.when(mockUser.getUsername()).thenReturn(existedUsername);
         Assertions.assertThrows(UsernameDuplicatedException.class, ()-> userService.insertUser(mockUser));
     }
     
     @Test
     void testValidateUsernameDuplicatedExist(){
-        Mockito.when(mockUserMapper.getUserByUsername("a")).thenReturn(Mockito.mock(User.class));
-        boolean validateUserA = userService.validateUsernameDuplicated("a");
+        Mockito.when(mockUserMapper.getUserByUsername(existedUsername)).thenReturn(Mockito.mock(User.class));
+        boolean validateUserA = userService.validateUsernameDuplicated(existedUsername);
         Assertions.assertFalse(validateUserA);
     }
     
     @Test
     void testValidateUsernameDuplicatedNotExist(){
-        Mockito.when(mockUserMapper.getUserByUsername("b")).thenReturn(null);
-        boolean validateUserB = userService. validateUsernameDuplicated("b");
+        Mockito.when(mockUserMapper.getUserByUsername(notExistedUsername)).thenReturn(null);
+        boolean validateUserB = userService. validateUsernameDuplicated(notExistedUsername);
         Assertions.assertTrue(validateUserB);
     }
     
