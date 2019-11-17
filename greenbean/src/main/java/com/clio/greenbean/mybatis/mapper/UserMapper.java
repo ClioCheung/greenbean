2fd0b79a8f836efb5eac9e9e682985eda34f4027
ClioChang <cliostill1@gmail.com>
2019-11-17 15:59:01 +0800

    测试 dev分支下实际增加一个分支： XXX-mybatis.xml

--- greenbean/src/main/java/com/clio/greenbean/mybatis/mapper/UserMapper.java
+++ greenbean/src/main/java/com/clio/greenbean/mybatis/mapper/UserMapper.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface UserMapper {
     
     // test version : XXX-mybatis.xml
+    // add  XXX-mybatis.xml 分支
     @SuppressWarnings("unused")
     User getUserQById(@Param("id") Integer id);
     
