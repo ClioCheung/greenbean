111b9c294ff92eb4b783938f6590c9ed71efcc7d
ClioChang <cliostill1@gmail.com>
2019-11-17 16:02:00 +0800

    测试 把XXX-mybatis.xml分支merge到dev分支,并delete XXX-mybatis.xml分支

--- greenbean/src/main/java/com/clio/greenbean/mybatis/mapper/UserMapper.java
+++ greenbean/src/main/java/com/clio/greenbean/mybatis/mapper/UserMapper.java
@@ -10,8 +10,9 @@ import java.util.List;
  */
 public interface UserMapper {
     
-    // test version : XXX-mybatis.xml
-    // add  XXX-mybatis.xml 分支
+    // 第一步 test version : XXX-mybatis.xml
+    // 第二步 add  XXX-mybatis.xml 分支
+    // 第三步 准备merge到dev分支,并delete XXX-mybatis.xml分支
     @SuppressWarnings("unused")
     User getUserQById(@Param("id") Integer id);
     
