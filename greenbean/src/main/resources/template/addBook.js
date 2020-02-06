77ab4a121702f911cb1deeafeb28f07781f46784
ClioChang <cliostill1@gmail.com>
2020-02-06 22:12:09 +0800

    10.1.9 Cancel按钮处理

--- greenbean/src/main/resources/template/addBook.js
+++ greenbean/src/main/resources/template/addBook.js
@@ -1,12 +1,14 @@
 (function () {
     $(function(){
-        debugger;
         const nextButton = $('#nextButton');
-        const cancelButton = $('#cancelButton');
         nextButton.on("click",function(){
             // TODO first step ： 验证ISBN
             $('.d-none').removeClass("d-none");
             $('#isbn').attr('readonly','');
-        })
+        });
+        const cancelButton = $('#cancelButton');
+        cancelButton.on('click',function () {
+            window.location.href="home";
+        });
     });
 })();
