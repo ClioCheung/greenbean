79a5d68a79f9a36865491cf52303340029d8da29
ClioChang <cliostill1@gmail.com>
2020-02-06 21:54:59 +0800

    10.1.8 Next按钮第一阶段处理

--- greenbean/src/main/resources/template/addBook.js
+++ greenbean/src/main/resources/template/addBook.js
@@ -1,1 +1,12 @@
-debugger;
+(function () {
+    $(function(){
+        debugger;
+        const nextButton = $('#nextButton');
+        const cancelButton = $('#cancelButton');
+        nextButton.on("click",function(){
+            // TODO first step ： 验证ISBN
+            $('.d-none').removeClass("d-none");
+            $('#isbn').attr('readonly','');
+        })
+    });
+})();
