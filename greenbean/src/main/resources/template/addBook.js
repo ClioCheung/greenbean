ed0189f2228cdc2bc6aca3bf16f1f1b1235cfaed
ClioChang <cliostill1@gmail.com>
2020-02-08 15:49:27 +0800

    10.1.10 加作者按钮处理-1

--- greenbean/src/main/resources/template/addBook.js
+++ greenbean/src/main/resources/template/addBook.js
@@ -1,14 +1,37 @@
 (function () {
+
     $(function(){
+        bindNextButtonClickEvent();
+        bindCancelButtonClickEvent();
+        bindAddButtonClickEvent()
+    });
+
+    function bindNextButtonClickEvent() {
         const nextButton = $('#nextButton');
         nextButton.on("click",function(){
             // TODO first step ： 验证ISBN
             $('.d-none').removeClass("d-none");
             $('#isbn').attr('readonly','');
         });
+    };
+
+    function bindCancelButtonClickEvent() {
         const cancelButton = $('#cancelButton');
         cancelButton.on('click',function () {
             window.location.href="home";
         });
-    });
+    };
+
+    function bindAddButtonClickEvent(){
+        const addButton = $('.addButton');
+        addButton.on('click',function(event){
+            debugger;
+            const target = $(event.currentTarget);
+            const dataNumber = target.attr("data-number");
+            // TODO to do
+        });
+
+    };
+
+
 })();
