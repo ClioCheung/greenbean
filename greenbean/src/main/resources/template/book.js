18c35279bd75075fef1fc09f7719761089e5a629
ClioChang <cliostill1@gmail.com>
2020-03-12 14:38:16 +0800

    11.4.4-2 用户评分的星星初始状态设置

--- greenbean/src/main/resources/template/book.js
+++ greenbean/src/main/resources/template/book.js
@@ -2,33 +2,41 @@
     $(function () {
         const ratingClickStar = $('#ratingClickStar');
         const ratingStarIntro = $('#ratingStarIntro');
-        const ratingStarIntroText = ['Very bad', 'Bad', 'Average', 'Good', 'Very good'];
-        ratingClickStar.on('mouseover',function (event) {
-            const target = $(event.target);
-            const imgIndex = target.data('index');
-            ratingClickStar.children().each(function (index, element) {
-                const img = $(element.firstElementChild);
-                if(index <= imgIndex){
-                    img.attr('src',ratingClickStar.attr('data-solid'));
-                } else {
-                    img.attr('src',ratingClickStar.attr('data-hollow'));
-                }
+        initRating(ratingClickStar, ratingStarIntro);
+        const dialogRatingClickStar = $('#dialogRatingClickStar');
+        const dialogRatingStarIntro = $('#dialogRatingStarIntro');
+        initRating(dialogRatingClickStar, dialogRatingStarIntro);
+        
+        function initRating(ratingClickStar, ratingStarIntro){
+            const ratingStarIntroText = ['', 'Very bad', 'Bad', 'Average', 'Good', 'Very good'];
+            ratingClickStar.on('mouseover',function (event) {
+                const imgIndex = $(event.target).data('index');
+                makeStarSolid(ratingClickStar, ratingStarIntro, imgIndex);
             });
-            ratingStarIntro.text(ratingStarIntroText[imgIndex]);
-        });
-    
-        ratingClickStar.on('mouseleave', function () {
-            ratingClickStar.children().each(function (index, element) {
-                $(element.firstElementChild).attr('src',ratingClickStar.attr('data-hollow'));
+            ratingClickStar.on('mouseleave', function () {
+                const imgIndex = ratingClickStar.data('rating');
+                makeStarSolid(ratingClickStar, ratingStarIntro, imgIndex);
             });
-            ratingStarIntro.text('');
+            ratingClickStar.triggerHandler('mouseleave');
+            
+            function makeStarSolid(ratingClickStar, ratingStarIntro, targetIndex){
+                ratingClickStar.children().each(function (index, element) {
+                    const img = $(element.firstElementChild);
+                    if (index < targetIndex) {
+                        img.attr('src', ratingClickStar.attr('data-solid'));
+                    } else {
+                        img.attr('src', ratingClickStar.attr('data-hollow'));
+                    }
+                });
+                ratingStarIntro.text(ratingStarIntroText[targetIndex]);
+            }
+        }
+        
+        $('#ratingDialog').on('show.bs.modal', function (event) {
+            const button = $(event.relatedTarget);
+            const recipient = button.data('status');
+            const modal = $(this);
+            modal.find('.modal-title>span').text(recipient);
         });
-    });
-    
-    $('#ratingDialog').on('show.bs.modal', function (event) {
-        const button = $(event.relatedTarget);
-        const recipient = button.data('status');
-        const modal = $(this);
-        modal.find('.modal-title>span').text(recipient);
     });
 })();
